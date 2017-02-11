f0f1bbb0e491940942da74eebef0a0c528a2b2e3
Will Price <will.price94@gmail.com>
2017-02-11 14:57:43 +0000

    Update run_jupyter.sh to new caffe path

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 JUPYTER="${JUPYTER:-jupyter}"
 JUPYTER_CONFIG="$HOME/jupyter-configs/caffe-excitation-bp-config.py"
 
-export PYTHONPATH="$SCRIPT_DIR/lib/:$PYTHONPATH"
+export PYTHONPATH="$HOME/lib/caffe/python:$PYTHONPATH"
 
 cd "$SCRIPT_DIR"
 "$JUPYTER" notebook --config="$JUPYTER_CONFIG"
